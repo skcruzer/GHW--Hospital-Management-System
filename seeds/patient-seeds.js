ef4543f931789aeb97c374677a40d5b2b34187e9
Hye Won Park <hyewonpark26@gmail.com>
2022-03-30 13:19:24 -0700

    creates patient seeds

---
+++ seeds/patient-seeds.js
@@ -0,0 +1,40 @@
+const { Employee } = require('../model')
+
+const employeesData = [
+  {
+    name: "Kien Hoang",
+    sex: "male",
+    dob: 20111127,
+    mobile: "123456789",
+    email: "test@gmail.com",
+    salary: 100000
+  },
+  {
+    name: "Sean Cruz",
+    sex: "male",
+    dob: 20111127,
+    mobile: "123456789",
+    email: "test@gmail.com",
+    salary: 100000
+  },
+  {
+    name: "Hye Won Park ",
+    sex: "male",
+    dob: 20111127,
+    mobile: "123456789",
+    email: "test@gmail.com",
+    salary: 100000
+  },
+  {
+    name: "Brendon Sanfer",
+    sex: "male",
+    dob: 20111127,
+    mobile: "123456789",
+    email: "test@gmail.com",
+    salary: 100000
+  }
+]
+
+const seedEmployees = () => Employee.bulkCreate(employeesData)
+
+module.exports = seedEmployees
