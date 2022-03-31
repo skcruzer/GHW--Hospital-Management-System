9e739f4c6c788706bb3765ac08ed35f4ae00cbd0
NGGKI <kien.dv2012@gmail.com>
2022-03-31 11:45:58 -0700

    update

--- seeds/index.js
+++ seeds/index.js
@@ -1,14 +1,17 @@
-const seedEmployees = require("./employee-seeds")
+const seedEmployees = require("./employee-seeds");
+const seedPatients = require("./patient-seeds");
 
-const sequelize = require("../config/connection")
+const sequelize = require("../config/connection");
 
 const seedAll = async () => {
-  await sequelize.sync({ force: true })
-  console.log('\n----- DATABASE SYNCED -----\n')
-  await seedEmployees()
-  console.log('\n----- EMPLOYEE SEEDED -----\n')
-  
-  process.exit(0)
-}
+  await sequelize.sync({ force: true });
+  console.log("\n----- DATABASE SYNCED -----\n");
+  await seedEmployees();
+  console.log("\n----- EMPLOYEE SEEDED -----\n");
+  await seedPatients();
+  console.log("\n----- PATIENT SEEDED -----\n");
 
-seedAll()
+  process.exit(0);
+};
+
+seedAll();
