ef2d3c1d9294b1394e67eb319a26d1bf6d63af59
Hye Won Park <hyewonpark26@gmail.com>
2022-04-01 11:34:27 -0700

    CREATE/post employee route

--- controller/employee.js
+++ controller/employee.js
@@ -31,7 +31,15 @@ router.get('/employees/:id', async ({body, params:{id}}, res) => {
   }
 });
 
-
+//CREATE an employee
+router.post('/employees', async ({ body }, res) => {
+  try {
+    const employee = await Employee.create(body);
+    res.status(200).json(employee);
+  } catch (err) {
+    res.status(400).json({ message: 'Woops! Something went wrong.' })
+  }
+})
 
 //DELETE an employee
 
