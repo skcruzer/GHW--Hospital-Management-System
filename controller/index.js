f22c37a5ca54fcf687e8081becce78316ab62225
skcruzer <96440269+skcruzer@users.noreply.github.com>
2022-04-01 13:24:55 -0700

    patient route

--- controller/index.js
+++ controller/index.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 
 router.use("/api", require("./employee.js"));
 router.use("/api", require("./diagnose"));
+router.use('/api', require("./patient.js"));
 
 module.exports = router;
